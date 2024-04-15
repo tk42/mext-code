8b32f4a079d90957666d7e71559b43750132049a
tk42 <nsplat@gmail.com>
2024-04-15 18:12:37 +0900

    Refactor read_by_subject function to include school parameter

--- mext-code/hasura.py
+++ mext-code/hasura.py
@@ -6,20 +6,20 @@ HASURA_URL = os.getenv("HASURA_URL")
 HASURA_SECRET = os.getenv("HASURA_SECRET")
 
 
-def read_by_subject(subject: str, goal: list[str], limit: int = 10):
+def read_by_subject(school: str, subject: str, goal: list[str], limit: int = 10):
     headers = {
         "Content-Type": "application/json",
         "x-hasura-admin-secret": HASURA_SECRET,
     }
     query = """
-    query MyQuery($subject: bpchar!, $goal: [bpchar!]!, $limit: Int!) {
+    query MyQuery($school: bpchar!, $subject: bpchar!, $goal: [bpchar!]!, $limit: Int!) {
         codes (
             where: {
                 version: {
                     _eq: "8"
                 },
                 school: {
-                    _eq: "3"
+                    _eq: $school
                 },
                 subject: {
                     _eq: $subject
@@ -38,7 +38,7 @@ def read_by_subject(subject: str, goal: list[str], limit: int = 10):
         }
     }
     """
-    variables = {"subject": subject, "goal": goal, "limit": limit}
+    variables = {"school": school, "subject": subject, "goal": goal, "limit": limit}
     result = httpx.post(
         HASURA_URL, json={"query": query, "variables": variables}, headers=headers
     )
--- mext-code/main.py
+++ mext-code/main.py
@@ -10,26 +10,53 @@ st.set_page_config(
     page_title="学習指導要領コード", layout="wide", initial_sidebar_state="expanded"
 )
 
-st.title("学習指導要領コード 自動割り当てシステム")
+st.title("学習指導要領コードそれっぽく割り当てツール")
 
 st.sidebar.title("ラベル対象条件")
 
+school = st.sidebar.selectbox(
+    "学校？",
+    ("2", "3", "4"),
+    format_func=lambda x: ["小学校", "中学校", "高等学校"][int(x) - 2],
+    index=1,
+    placeholder="学校を選択してください",
+)
+
 subject = st.sidebar.selectbox(
     "どの教科？",
-    ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"),
+    ("0", "1", "2", "3", "4", "5", "6", "7", "8"),
+    format_func=lambda x: [
+        "総則",
+        "国語",
+        "社会",
+        "地理歴史",
+        "公民",
+        "算数・数学",
+        "理科",
+        "生活",
+        "音楽",
+    ][int(x)],
     index=5,
     placeholder="教科を選択してください",
 )
 
 goal = st.sidebar.multiselect(
     "目標区分",
-    ["0", "1", "2", "3", "4", "5", "6", "7"],
-    default=["4", "5", "6", "7"],
+    ["0", "1", "2", "3", "4", "5"],
+    format_func=lambda x: [
+        "その他",
+        "目標（大項目）",
+        "目標及び内容（大項目）",
+        "指導計画の作成と内容の取り扱い（大項目）",
+        "内容（大項目）",
+        "各科目",
+    ][int(x)],
+    default=["4", "5"],
 )
 
 limit = st.sidebar.number_input("最大ラベル数", min_value=1, max_value=10, value=3)
 
-data = read_by_subject(subject, goal=goal, limit=None)
+data = read_by_subject(school=school, subject=subject, goal=goal, limit=None)
 st.sidebar.write(f"{len(data['data']['codes'])} 件のラベルが対象")
 
 st.sidebar.selectbox("モデル選択", ("gpt-3.5-turbo", "gpt-4", "gpt-4-turbo"), index=0)
@@ -52,7 +79,7 @@ def make_instruction(_input, _labels, limit):
 
 
 def on_submit():
-    data = read_by_subject(subject, goal=goal, limit=None)
+    data = read_by_subject(school=school, subject=subject, goal=goal, limit=None)
     labels = data["data"]["codes"]
     instruction = make_instruction(input_txt, labels, limit)
     completion = client.chat.completions.create(
